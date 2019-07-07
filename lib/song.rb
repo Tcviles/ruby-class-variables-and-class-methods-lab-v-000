d532b6743dfcc595fc10f4694b304dac3e93bd8c
Thomas Viles <thomas.c.viles.jr@gmail.com>
2019-07-07 17:44:24 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,10 @@
 class Song
   attr_reader :name, :artist, :genre
-  
+
   @@count = 0
   @@artists = []
   @@genres = []
-  
+
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
